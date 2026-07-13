93ef197515b94381cc179644d1852e9f68194952
Bidhanshu01 <bidhanshuthakur01@gmail.com>
2026-07-14 03:28:39 +0530

    learned events in js

---
+++ event.js
@@ -0,0 +1,28 @@
+let button =document.getElementById("btn")
+
+// button.addEventListener("click", () => {  // when the button is clicked, the function inside the event listener will be executed
+//     // Code to change content
+//    // alert("Button clicked!")  // this will show an alert box with the message "Button clicked!"
+
+//    // document.querySelector(".box").innerHTML = "Enjoy your click !"  // this will change the content of the element with class "box" to "Content changed!"
+
+   
+
+// })
+
+
+
+// button.addEventListener("dbclick", () => {  // when the button is double clicked, the function inside the event listener will be executed
+//     document.querySelector(".box").innerHTML = "Enjoy your double click !"  // this will change the content of the element with class "box" to "Content changed!"
+
+// })
+
+
+
+// button.addEventListener("contextmenu", () => {  // when the button is right-clicked, the function inside the event listener will be executed
+//     document.querySelector(".box").innerHTML = "Enjoy your context menu !"  // this will change the content of the element with class "box" to "Content changed!"
+
+// })
+
+
+// we will find more event in enternets like that tou cust cahnage event name and content
